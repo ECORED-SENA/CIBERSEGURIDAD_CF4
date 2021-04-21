a0d77cc8af6d9cefd29534e8df016993068a5f4e
Jhon Jairo Urueta Alvarez <47787645+jhonuru@users.noreply.github.com>
2021-04-21 11:54:10 -0500

    Correcciones 1
    Ajuste de imágenes y glosario.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -7,13 +7,13 @@
       .titulo-principal__numero
         .h3 i
       .h3 Introducción
+    .imagen.mb-4
+      figure
+        img(src='@/assets/curso/img5.png', alt='Texto que describa la imagen')
     p.mb-4 Los sistemas de gestión de seguridad de la información o SGSI es un término que se utiliza en la Norma Internacional ISO 27001 para referirse al conjunto de políticas, normas y controles que se aplican en una organización para garantizar el cumplimiento de los pilares de la seguridad de la información como lo son la integridad, la disponibilidad y la confidencialidad.
     p.mb-4 es el estándar internacional para la seguridad de la información que establece la especificación para un sistema de gestión de seguridad de la información (SGSI).
     p.mb-4 El enfoque de mejores prácticas del estándar del sistema de gestión de seguridad de la información ayuda a las organizaciones a gestionar su seguridad de la información dirigiéndose a las personas y los procesos, así como a la tecnología.
-    p.mb-4 La certificación acreditada de forma independiente de la norma ISO 27001 es reconocida en todo el mundo como una indicación de que su SGSI está alineado con las mejores prácticas de seguridad de la información.
-    .imagen-titulo--izq
-      figure
-        img(src='@/assets/curso/img2.jpg', alt='Texto que describa la imagen')
+    p La certificación acreditada de forma independiente de la norma ISO 27001 es reconocida en todo el mundo como una indicación de que su SGSI está alineado con las mejores prácticas de seguridad de la información.
 
 </template>
 
